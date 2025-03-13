2377242168830ef5d8197d1a81a210c0bbdddbaf
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-03-14 01:44:43 +0600

    videos

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ function LoadCateregory(){
     .then(data =>displayCategoris(data.categories))
 }
 
+// video loaded
 
+function Loadvideo(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data=>DisplayVideo(data.videos))
+}
+
+// display category btn
 function displayCategoris(categories){
     const categoryContainer=document.getElementById('category-container');
     for(cat of categories){
@@ -20,4 +28,38 @@ function displayCategoris(categories){
         categoryContainer.append(categoryDiv);
     }
 }
+
+
+// display videos
+
+function DisplayVideo(videos){
+    
+    const videoContainer=document.getElementById('video-container');
+    for(const video of videos){
+
+        console.log(video)
+        const videoDiv=document.createElement("div");
+        videoDiv.innerHTML=`
+        <div class="card bg-base-100  shadow-sm">
+  <figure>
+    <img
+    class="w-full h-full"
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+        `
+        videoContainer.append(videoDiv);
+    }
+}
 LoadCateregory();
+
+Loadvideo();
